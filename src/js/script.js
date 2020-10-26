aa2dd2b7bdd7f3da2ec0fd60523d91a8b0a0d644
VibeTattooCrew <crewvibetattoo@gmail.com>
2020-10-25 21:34:58 -0300

    commit vibe tattoo

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+$(function(){
+
+    //scroll
+
+    $(".link-scroll").on("click", function(e){
+        e.preventDefault()
+        var id = $(this).attr("href") 
+        var dist = $(id).offset().top 
+        $("body, html").animate({scrollTop: dist - 100}, 900)
+    })
+
+ 
+    //ajax
+    
+    $('#grafite').on("click", function(e){
+        e.preventDefault()
+        $.get("grafites.html", function(data){
+            $('#conteudo').html(data)
+        })    
+    })
+
+    $('#quadro').on("click", function(e){
+        e.preventDefault()
+        $.get("quadros.html", function(data){
+            $('#conteudo').html(data)
+        })
+    })
+    $('#desenho').on("click", function(e){
+        e.preventDefault()
+        $.get("desenhos.html", function(data){
+            $('#conteudo').html(data)
+        })
+
+    })
+    
+    $(document).ready(function(){
+        $.get("grafites.html", function(data){
+            $('#conteudo').html(data)
+        })    
+    })
+
+})
